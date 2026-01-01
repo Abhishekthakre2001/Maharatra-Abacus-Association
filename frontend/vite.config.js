009651221e6b5f1566608a09c8b63c0a085b9b42
Abhishek Thakre <abhishekpthakre2023@gmail.com>
2026-01-01 17:07:17 +0530

    as it is

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,5 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/The_Wonder_TezzDimag_Abacus_Claasess/',
   plugins: [react(), tailwindcss()],
 });
