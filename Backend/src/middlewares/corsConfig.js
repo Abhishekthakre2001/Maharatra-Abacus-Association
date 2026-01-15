a0e2f4e297300fb80db26fb79152e33361e77056
Abhishek Thakre <abhishekpthakre2023@gmail.com>
2026-01-15 10:28:30 +0530

    ok

--- Backend/src/middlewares/corsConfig.js
+++ Backend/src/middlewares/corsConfig.js
@@ -1,26 +1,22 @@
 const cors = require("cors");
 
 const allowedOrigins = [
-  "http://localhost:3000",
+  "http://localhost:5173",
   "https://abacus.deveraa.com",
-    "https://apiabacus.deveraa.com",
-  "http://localhost:5173" // Vite
+  "http://192.168.1.7:5173/"
 ];
 
 const corsOptions = {
   origin: function (origin, callback) {
-    // allow requests with no origin (Postman, mobile apps)
-    if (!origin) return callback(null, true);
-
-    if (allowedOrigins.includes(origin)) {
+    if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
       callback(new Error("Not allowed by CORS"));
     }
   },
-  credentials: true, // ✅ REQUIRED
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
 };
 
 module.exports = cors(corsOptions);
