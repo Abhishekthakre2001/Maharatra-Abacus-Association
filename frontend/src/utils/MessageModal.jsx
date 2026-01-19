aa3573820220170cf8bf9c1f260145f578c457bf
Abhishek Thakre <abhishekpthakre2023@gmail.com>
2026-01-19 23:59:24 +0530

    ok

--- frontend/src/utils/MessageModal.jsx
+++ frontend/src/utils/MessageModal.jsx
@@ -16,7 +16,7 @@ const MessageModal = ({
 
   return (
     <div
-      className="fixed inset-0 bg-black/40 backdrop-blur-sm flex justify-center items-center z-50 animate-fadeIn"
+      className="fixed inset-0 bg-black/40 backdrop-blur-sm flex justify-center items-center z-100 animate-fadeIn"
       onClick={onClose}
     >
       <div
