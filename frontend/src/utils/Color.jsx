0625923475d24d83edc5d7b088cb0e751b1bc7c2
Abhishek Thakre <abhishek_thakre@webbetasolutions.com>
2026-03-10 23:51:02 +0530

    default logo remove

--- frontend/src/utils/Color.jsx
+++ frontend/src/utils/Color.jsx
@@ -12,7 +12,7 @@ const colors = {
   Client: {
     brand_name: "DevEraa",
     brand_sub_title: "Grow with Deveraa",
-    logo_url: "https://deveraa.com/assets/favicon.png"
+    logo_url: "https://media.istockphoto.com/id/2173059563/vector/coming-soon-image-on-white-background-no-photo-available.jpg?s=612x612&w=0&k=20&c=v0a_B58wPFNDPULSiw_BmPyhSNCyrP_d17i2BPPyDTk="
   },
 
   examScheduleCard: {
