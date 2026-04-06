673695895d62fcc927705ce29fb4f6da483e6c50
Abhishek Thakre <abhishek_thakre@webbetasolutions.com>
2026-04-06 23:57:16 +0530

    cache added

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # The_Wonder_TezzDimag_Abacus_Claasess
 The Wonder TezzDimag Abacus Claasess
+
+
+C:\k6\k6.exe run .\load-question-paper.js               
--- load-question-paper.js
+++ load-question-paper.js
@@ -9,7 +9,10 @@ export const options = {
 const BASE_URL = 'http://localhost:5000';
 
 export default function () {
-  const res = http.get(`${BASE_URL}/questions/paperset?level=1&createdby=1&set=A`);
+  const res = http.get(`${BASE_URL}/results/check?user_id=73&exam_id=2`);
+  //  const res = http.get(`${BASE_URL}/questions/level-wise-sets?level=0&createdby=50`);
+  // const res = http.get(`${BASE_URL}/exam-schedule/studentexam?level=0&createdby=50`);
+  //  const res = http.get(`${BASE_URL}/questions/paperset?level=1&createdby=1&set=A`);
 
   check(res, {
     'status 200': (r) => r.status === 200,
